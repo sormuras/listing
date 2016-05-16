3c60d7397afc936c0e9219410f1c7c8090d168cb
Christian Stein <sormuras@gmail.com>
2016-05-16 20:46:25 +0200

    Compiling multiple unit in one go.

--- src/test/com/github/sormuras/listing/CompilationTest.java
+++ src/test/com/github/sormuras/listing/CompilationTest.java
@@ -1,9 +1,9 @@
 package com.github.sormuras.listing;
 
+import static java.util.Collections.emptyList;
 import static org.junit.Assert.assertNotEquals;
 
 import java.net.URI;
-
 import javax.tools.JavaFileObject;
 
 import org.junit.Test;
@@ -11,8 +11,17 @@ import org.junit.Test;
 public class CompilationTest {
 
   @Test
-  public void charContentFileObject() throws Exception {
+  public void lastModified() throws Exception {
     JavaFileObject jfo = new Compilation.CharContentFileObject(URI.create("abc"), "abc");
     assertNotEquals(0L, jfo.getLastModified());
+  }
+
+  @Test
+  public void multipleUnitsWithDependingOnEachOther() throws Exception {
+    JavaUnit a = new JavaUnit("a");
+    a.declareClass("A").addModifier("public");
+    JavaUnit b = new JavaUnit("b");
+    b.declareClass("B").setSuperClass(ClassType.of("a", "A"));
+    Compilation.compile(getClass().getClassLoader(), emptyList(), emptyList(), a, b);
   }
 }
