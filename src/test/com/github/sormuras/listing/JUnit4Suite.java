183401edd943332dcb27705b249d8e542d0ebb8c
Christian Stein <sormuras@gmail.com>
2016-06-18 20:22:41 +0200

    Test suite renamed.

---
+++ src/test/com/github/sormuras/listing/JUnit4Suite.java
@@ -0,0 +1,15 @@
+package com.github.sormuras.listing;
+
+import org.junit.gen5.junit4.runner.JUnit5;
+import org.junit.gen5.junit4.runner.Packages;
+import org.junit.runner.RunWith;
+
+/**
+ * JUnit4Suite will discover and run all JUnit 5 tests in the {@code com.github.sormuras.listing}
+ * package and its subpackages.
+ *
+ * @see <a href="http://junit.org/junit5/docs/snapshot/user-guide/#test-suite">Test Suite</a>
+ */
+@RunWith(JUnit5.class)
+@Packages("com.github.sormuras.listing")
+public class JUnit4Suite {}
