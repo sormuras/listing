db4d8c031228cde80896610f08f001f6ac252f08
Christian Stein <sormuras@gmail.com>
2016-06-18 11:23:34 +0200

    Added properties.

--- src/java/com/github/sormuras/listing/Name.java
+++ src/java/com/github/sormuras/listing/Name.java
@@ -3,6 +3,41 @@ package com.github.sormuras.listing;
 /**
  * Names are used to refer to entities declared in a program.
  *
- * @see https://docs.oracle.com/javase/specs/jls/se8/html/jls-6.html
+ * @see JLS: <a href="https://docs.oracle.com/javase/specs/jls/se8/html/jls-6.html">Names</a>
  */
-public class Name {}
+public final class Name {
+
+  private final String[] identifiers;
+  private final String qualified;
+
+  public Name(String... identifiers) {
+    this.identifiers = identifiers;
+    this.qualified = String.join(".", getIdentifiers());
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    if (this == other) return true;
+    if (other == null || getClass() != other.getClass()) return false;
+    return hashCode() == other.hashCode();
+  }
+
+  public String[] getIdentifiers() {
+    return identifiers;
+  }
+
+  public String getQualified() {
+    return qualified;
+  }
+
+  @Override
+  public int hashCode() {
+    return qualified.hashCode();
+  }
+
+  @Override
+  public String toString() {
+    return "Name[" + getQualified() + "]";
+  }
+
+}
--- src/test/com/github/sormuras/listing/NameTest.java
+++ src/test/com/github/sormuras/listing/NameTest.java
@@ -1,12 +1,34 @@
 package com.github.sormuras.listing;
 
-import org.junit.gen5.api.Assertions;
+import static org.junit.gen5.api.Assertions.assertEquals;
+import static org.junit.gen5.api.Assertions.assertNotEquals;
+import static org.junit.gen5.api.Assertions.assertTrue;
+
 import org.junit.gen5.api.Test;
 
 class NameTest {
 
   @Test
   void create() {
-    Assertions.assertNotNull(new Name());
+    assertEquals("abc", new Name("abc").getIdentifiers()[0]);
+    assertEquals("abc", new Name("abc").getQualified());
+  }
+
+  @Test
+  void equals() {
+    Name name = new Name("abc");
+    assertTrue(name.equals(name));
+    assertEquals(name, name);
+    assertEquals(name, new Name("abc"));
+    assertNotEquals(name, null);
+    assertNotEquals(name, new Object());
+    assertNotEquals(name, new Name("xyz"));
+  }
+
+  @Test
+  void string() {
+    assertEquals("Name[abc]", new Name("abc").toString());
+    assertEquals("Name[a.b]", new Name("a", "b").toString());
+    assertEquals("Name[a.b.C]", new Name("a", "b", "C").toString());
   }
 }
