4920d23796b428107b660806c7c64120b8e8e7f5
Christian Stein <sormuras@gmail.com>
2016-08-02 11:56:00 +0200

    Testing TypeArgument constructor and getters.

--- src/test/com/github/sormuras/listing/type/TypeArgumentTest.java
+++ src/test/com/github/sormuras/listing/type/TypeArgumentTest.java
@@ -1,11 +1,23 @@
 package com.github.sormuras.listing.type;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.expectThrows;
 
 import org.junit.jupiter.api.Test;
 
 class TypeArgumentTest {
+
+  @Test
+  void constructor() {
+    TypeArgument argumentWithReference = new TypeArgument(Object.class);
+    assertNotNull(argumentWithReference.getReference());
+    assertNull(argumentWithReference.getWildcard());
+    TypeArgument argumentWithWildcard = new TypeArgument(new WildcardType());
+    assertNull(argumentWithWildcard.getReference());
+    assertNotNull(argumentWithWildcard.getWildcard());
+  }
 
   @Test
   void constructorFailsWithWrongJavaType() {
