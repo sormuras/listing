f035361345c2a4e14018d86b15592cb9c6422f1e
Christian Stein <sormuras@gmail.com>
2016-08-08 17:26:58 +0200

    Byte array to String tools added.

--- src/test/com/github/sormuras/listing/Tests.java
+++ src/test/com/github/sormuras/listing/Tests.java
@@ -8,6 +8,7 @@ import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -32,7 +33,9 @@ public interface Tests {
     try {
       String expected = listable.list();
       Assertions.assertEquals(expected, listable.list());
-      String actual = ((Listable) convertFromBytes(convertToBytes(listable))).list();
+      byte[] bytes = convertToBytes(listable);
+      Object converted = convertFromBytes(bytes);
+      String actual = ((Listable) converted).list();
       Assertions.assertEquals(expected, actual);
     } catch (Exception e) {
       Assertions.fail(e.toString());
@@ -68,5 +71,101 @@ public interface Tests {
     ClassLoader loader = type.getClassLoader();
     Class<?>[] interfaces = {type};
     return type.cast(Proxy.newProxyInstance(loader, interfaces, handler));
+  }
+
+  /** Same as: <code>toString(bytes, 16, 16)</code>. */
+  static String toString(byte[] bytes) {
+    return toString(bytes, 16, 16);
+  }
+
+  /** Same as: <code>toString(ByteBuffer.wrap(bytes), bytesPerLine, maxLines)</code>. */
+  static String toString(byte[] bytes, int bytesPerLine, int maxLines) {
+    return toString(ByteBuffer.wrap(bytes), bytesPerLine, maxLines);
+  }
+
+  /** Same as: <code>toString(buffer, 16, 16)</code>. */
+  static String toString(ByteBuffer buffer) {
+    return toString(buffer, 16, 16);
+  }
+
+  static String toString(ByteBuffer buffer, int bytesPerLine, int maxLines) {
+    return toString(new StringBuilder(), buffer, bytesPerLine, maxLines);
+  }
+
+  static String toString(StringBuilder builder, ByteBuffer buffer) {
+    return toString(builder, buffer, 16, 16);
+  }
+
+  static String toString(StringBuilder builder, ByteBuffer buffer, int bytesPerLine, int maxLines) {
+    final boolean INCLUDE_SEGMENT_NUMBERS = true;
+    final boolean INCLUDE_VIEW_HEX = true;
+    final boolean INCLUDE_VIEW_ASCII = true;
+    final int BLOCK_LENGTH = 4;
+    final char BLOCK_SEPARATOR = ' ';
+    int i, j, n, k, line;
+    builder.append(buffer).append(" {\n");
+    line = 0;
+    for (n = 0; n < buffer.remaining(); n += bytesPerLine, line++) {
+      // builder.append(" ");
+      if (line >= maxLines) {
+        int omitted = buffer.remaining() - n;
+        builder.append("...(");
+        builder.append(omitted);
+        builder.append(" byte");
+        builder.append(omitted != 1 ? "s" : "");
+        builder.append(" omitted)\n");
+        break;
+      }
+      if (INCLUDE_SEGMENT_NUMBERS) {
+        String segment = Integer.toHexString(n).toUpperCase();
+        for (j = 0, k = 4 - segment.length(); j < k; j++) {
+          builder.append('0');
+        }
+        builder.append(segment).append(" | ");
+      }
+      if (INCLUDE_VIEW_HEX) {
+        for (i = n; i < n + bytesPerLine && i < buffer.remaining(); i++) {
+          String s = Integer.toHexString(buffer.get(i) & 255).toUpperCase();
+          if (s.length() == 1) {
+            builder.append('0');
+          }
+          builder.append(s).append(' ');
+          if (i % bytesPerLine % BLOCK_LENGTH == BLOCK_LENGTH - 1 && i < n + bytesPerLine - 1) {
+            builder.append(BLOCK_SEPARATOR);
+          }
+        }
+        while (i < n + bytesPerLine) {
+          builder.append("   ");
+          if (i % bytesPerLine % BLOCK_LENGTH == BLOCK_LENGTH - 1 && i < n + bytesPerLine - 1) {
+            builder.append(BLOCK_SEPARATOR);
+          }
+          i++;
+        }
+        builder.append('|').append(' ');
+      }
+      if (INCLUDE_VIEW_ASCII) {
+        for (i = n; i < n + bytesPerLine && i < buffer.remaining(); i++) {
+          int v = buffer.get(i) & 255;
+          if (v > 127 || Character.isISOControl((char) v)) {
+            builder.append('.');
+          } else {
+            builder.append((char) v);
+          }
+          if (i % bytesPerLine % BLOCK_LENGTH == BLOCK_LENGTH - 1 && i < n + bytesPerLine - 1) {
+            builder.append(BLOCK_SEPARATOR);
+          }
+        }
+        while (i < n + bytesPerLine) {
+          builder.append(' ');
+          if (i % bytesPerLine % BLOCK_LENGTH == BLOCK_LENGTH - 1 && i < n + bytesPerLine - 1) {
+            builder.append(BLOCK_SEPARATOR);
+          }
+          i++;
+        }
+      }
+      builder.append('\n');
+    }
+    builder.append("}");
+    return builder.toString();
   }
 }
