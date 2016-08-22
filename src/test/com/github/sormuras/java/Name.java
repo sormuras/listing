40e103c2a0f83be2ad9376673ce5a0272d604e3e
Christian Stein <sormuras@gmail.com>
2016-08-23 00:47:32 +0200

    Major Name refactoring.

--- src/test/com/github/sormuras/java/Name.java
+++ src/test/com/github/sormuras/java/Name.java
@@ -1,37 +1,56 @@
 package com.github.sormuras.java;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.IntPredicate;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+import javax.lang.model.SourceVersion;
 
-public class Name {
+public final class Name {
+
+  public static final Pattern DOT = Pattern.compile("\\.");
 
   public static Name name(Class<?> type) {
-    String packageName = type.getPackage().getName();
-    String canonical = type.getCanonicalName();
-    return new Name(packageName.split("\\.").length, canonical.split("\\."));
+    String[] packageNames = DOT.split(type.getName()); // java[.]lang[.]Thread$State
+    String[] identifiers = DOT.split(type.getCanonicalName()); // java[.]lang[.]Thread[.]State
+    return new Name(packageNames.length - 1, Arrays.asList(identifiers));
+  }
+
+  public static Name name(Enum<?> constant) {
+    Name declaringName = name(constant.getDeclaringClass());
+    List<String> names = new ArrayList<>(declaringName.size + 1);
+    names.addAll(declaringName.identifiers);
+    names.add(constant.name());
+    return new Name(declaringName.packageLevel, names);
+  }
+
+  public static Name name(List<String> names) {
+    assert names.stream().allMatch(SourceVersion::isName) : "non-name in " + names;
+    int size = names.size();
+    IntPredicate uppercase = index -> Character.isUpperCase(names.get(index).codePointAt(0));
+    int level = IntStream.range(0, size).filter(uppercase).findFirst().orElse(size);
+    return new Name(level, names);
   }
 
   public static Name name(String... identifiers) {
-    int index = identifiers.length;
-    for (int i = 0; i < identifiers.length; i++) {
-      if (Character.isUpperCase(identifiers[i].charAt(0))) {
-        index = i;
-        break;
-      }
-    }
-    return new Name(index, identifiers);
+    return name(Arrays.asList(identifiers));
   }
 
   private final String canonical;
-
-  private final String[] identifiers;
-  private final int packageIndex;
+  private final List<String> identifiers;
+  private final int packageLevel;
   private final String packageName;
+  private final int size;
 
-  public Name(int packageIndex, String... identifiers) {
-    this.packageIndex = packageIndex;
-    this.identifiers = identifiers;
+  private Name(int packageLevel, List<String> identifiers) {
+    this.packageLevel = packageLevel;
+    this.identifiers = Collections.unmodifiableList(identifiers);
     this.canonical = String.join(".", identifiers);
-    this.packageName = String.join(".", Arrays.asList(identifiers).subList(0, packageIndex));
+    this.packageName = String.join(".", identifiers.subList(0, packageLevel));
+    this.size = identifiers.size();
   }
 
   public String canonical() {
@@ -39,20 +58,60 @@ public class Name {
   }
 
   public Name enclosing() {
-    if (identifiers.length <= 1) {
-      throw new IllegalArgumentException();
+    if (!isEnclosed()) {
+      throw new IllegalStateException(String.format("not enclosed: '%s'", this));
     }
-    String[] identifiers = new String[this.identifiers.length - 1];
-    System.arraycopy(this.identifiers, 0, identifiers, 0, identifiers.length);
-    return new Name(Math.min(packageIndex, identifiers.length), identifiers);
+    int shrinkedByOne = size - 1;
+    int newPackageLevel = Math.min(packageLevel, shrinkedByOne);
+    return new Name(newPackageLevel, identifiers.subList(0, shrinkedByOne));
   }
 
-  public String[] identifiers() {
+  @Override
+  public boolean equals(Object other) {
+    if (this == other) {
+      return true;
+    }
+    if (other == null || getClass() != other.getClass()) {
+      return false;
+    }
+    return hashCode() == other.hashCode();
+  }
+
+  @Override
+  public int hashCode() {
+    return canonical.hashCode();
+  }
+
+  public List<String> identifiers() {
     return identifiers;
+  }
+
+  public boolean isEnclosed() {
+    return size > 1;
+  }
+
+  public boolean isJavaLangObject() {
+    return size == 3 && "java.lang.Object".equals(canonical);
+  }
+
+  public boolean isJavaLangPackage() {
+    return packageLevel == 2 && "java.lang".equals(packageName);
+  }
+
+  public String lastName() {
+    return identifiers.get(size - 1);
   }
 
   public String packageName() {
     return packageName;
+  }
+
+  public Iterable<String> simpleNames() {
+    return identifiers.subList(packageLevel, size);
+  }
+
+  public int size() {
+    return size;
   }
 
   @Override
--- src/test/com/github/sormuras/java/NameTest.java
+++ src/test/com/github/sormuras/java/NameTest.java
@@ -1,32 +1,73 @@
 package com.github.sormuras.java;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 class NameTest {
 
   @Test
-  void test() {
-    Assertions.assertEquals("a", Name.name("a").packageName());
-    Assertions.assertEquals("a", Name.name("a").canonical());
-    Assertions.assertEquals("a.b", Name.name("a.b").packageName());
-    Assertions.assertEquals("a.b", Name.name("a.b").canonical());
-    Assertions.assertEquals("a.b", Name.name("a.b", "C").packageName());
-    Assertions.assertEquals("a.b.C", Name.name("a.b", "C").canonical());
-    Assertions.assertEquals("java.lang", Name.name(Object.class).packageName());
-    Assertions.assertEquals("java.lang.Object", Name.name(Object.class).canonical());
+  void array() {
+    assertEquals("int[][]", Name.name(int[][].class).canonical());
   }
 
   @Test
-  void enclosing() {
+  void primitive() {
+    assertEquals("void", Name.name(void.class).canonical());
+  }
+
+  @Test
+  void enclosed() {
+    Name.name(void.class);
     Name state = Name.name(Thread.State.class);
-    Assertions.assertEquals("java.lang.Thread.State", state.canonical());
+    assertEquals("java.lang.Thread.State", state.canonical());
+    assertTrue(state.isEnclosed());
+    assertTrue(state.isJavaLangPackage());
+
     Name thread = state.enclosing();
-    Assertions.assertEquals("java.lang.Thread", thread.canonical());
+    assertEquals("java.lang.Thread", thread.canonical());
+    assertTrue(thread.isEnclosed());
+    assertTrue(state.isJavaLangPackage());
+
     Name lang = thread.enclosing();
-    Assertions.assertEquals("java.lang", lang.canonical());
+    assertEquals("java.lang", lang.canonical());
+    assertTrue(lang.isEnclosed());
+    assertTrue(state.isJavaLangPackage());
+
     Name java = lang.enclosing();
-    Assertions.assertEquals("java", java.canonical());
-    Assertions.assertThrows(IllegalArgumentException.class, () -> java.enclosing());
+    assertEquals("java", java.canonical());
+    assertFalse(java.isEnclosed());
+    assertFalse(java.isJavaLangPackage());
+    assertThrows(IllegalStateException.class, () -> java.enclosing());
+  }
+
+  @Test
+  void name() {
+    assertEquals("A", Name.name("A").canonical());
+    assertEquals("", Name.name("A").packageName());
+    assertEquals("A", Name.name("A").lastName());
+
+    assertEquals("a", Name.name("a").canonical());
+    assertEquals("a", Name.name("a").packageName());
+    assertEquals("a", Name.name("a").lastName());
+
+    assertEquals("a.b", Name.name("a", "b").canonical());
+    assertEquals("a.b", Name.name("a", "b").packageName());
+    assertEquals("b", Name.name("a", "b").lastName());
+
+    assertEquals("a.b.C", Name.name("a", "b", "C").canonical());
+    assertEquals("a.b", Name.name("a", "b", "C").packageName());
+    assertEquals("C", Name.name("a", "b", "C").lastName());
+
+    assertEquals("java.lang.Object", Name.name(Object.class).canonical());
+    assertEquals("java.lang", Name.name(Object.class).packageName());
+    assertEquals("Object", Name.name(Object.class).lastName());
+
+    assertEquals("java.lang.Thread.State.NEW", Name.name(Thread.State.NEW).canonical());
+    assertEquals("java.lang", Name.name(Thread.State.NEW).packageName());
+    assertEquals("NEW", Name.name(Thread.State.NEW).lastName());
   }
 }
